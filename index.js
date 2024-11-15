1f3e2dc702f64e3ca3b990a3c62066b3b4113015
zoiebackman <zoiebackman@icloud.com>
2024-11-15 13:45:30 +0100

    skapat loop för div (#cities)

--- index.js
+++ index.js
@@ -9,21 +9,41 @@ const cityBox = document.querySelector("#cities");
 const cityTable = document.querySelector("#table");
 const title = document.querySelector("title");
 // Recommended: Ask for the city name and then the rest of the code
-
-const vilkenCity = prompt("skriv en stad?");
+//prompt
+const enteredCity = prompt("skriv en stad?");
 
 let cityWasFound = false;
 for (cityKey in cities) {
-    if (vilkenCity == cities[cityKey.name]) {
+    if (enteredCity == cities[cityKey].name) {
         cityWasFound = true;
-        cityTitle.innerHTML += `${vilkenCity} (${cities[cityKey].country})`;
-        tabName.innerHTML = enteredCity;
+        cityTitle.innerHTML += `${enteredCity} (${cities[cityKey].country})`;
+        title.innerHTML = enteredCity;
         break; // loopen slutar och behöver inte fortsätta
     }
 }
 if (cityWasFound == false) {
-    cityTitle.innerHTML += vilkenCity + " finns inte i databasen";
+    cityTitle.innerHTML += enteredCity + " finns inte i databasen";
     cityDetails.innerHTML = "";
     tabName.innerHTML = "Not Found";
 }
 
+for ( let i = 0; i <= 38; i++) {
+    let cityBoxP = document.createElement("p");
+    cityBoxP.classList.add("cityBox");
+    cityBoxP.textContent = cities[i].name;
+    cityBox.appendChild(cityBoxP);
+
+    if (enteredCity == cities[i].name) {
+        cityBoxP.classList.add("target");
+    }
+}
+
+
+
+//loop
+
+//grid
+//h2 ändras utifrån stad
+//h2 ska försvinna vid en icke databaserad stad
+//h3 ska ändras vid rätt och fel stad
+//funktion
