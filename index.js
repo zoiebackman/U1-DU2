82335ff086d9802e63ec4135449392a4b70a2ec6
zoiebackman <zoiebackman@icloud.com>
2024-11-15 12:52:42 +0100

    uppdatering javascript

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const cityTitle = document.querySelector("h2");
 const cityDetails = document.querySelector("h3");
 const cityClosest = document.querySelector("#closest");
 const cityFurthest = document.querySelector("#furthest");
-const cities = document.querySelector("#cities");
+const cityBox = document.querySelector("#cities");
 const cityTable = document.querySelector("#table");
 const title = document.querySelector("title");
 // Recommended: Ask for the city name and then the rest of the code
 
-const vilkenCity = prompt ("skriv en stad?");
+const vilkenCity = prompt("skriv en stad?");
 
-
+let cityWasFound = false;
+for (cityKey in cities) {
+    if (vilkenCity == cities[cityKey.name]) {
+        cityWasFound = true;
+        cityTitle.innerHTML += `${vilkenCity} (${cities[cityKey].country})`;
+        tabName.innerHTML = enteredCity;
+        break; // loopen slutar och behöver inte fortsätta
+    }
+}
+if (cityWasFound == false) {
+    cityTitle.innerHTML += vilkenCity + " finns inte i databasen";
+    cityDetails.innerHTML = "";
+    tabName.innerHTML = "Not Found";
+}
 
