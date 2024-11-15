49f68db94d989f0a7448ee95295622d48d9db25a
zoiebackman <zoiebackman@icloud.com>
2024-11-15 11:02:24 +0100

    skapat variabler

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Recommended: All functions declared here
 
 // Recommended: constants with references to existing HTML-elements
-
+const cityTitle = document.querySelector("h2");
+const cityDetails = document.querySelector("h3");
+const cityClosest = document.querySelector("#closest");
+const cityFurthest = document.querySelector("#furthest");
+const cities = document.querySelector("#cities");
+const cityTable = document.querySelector("#table");
+const title = document.querySelector("title");
 // Recommended: Ask for the city name and then the rest of the code
+
+const vilkenCity = prompt ("skriv en stad?");
+
+
 
